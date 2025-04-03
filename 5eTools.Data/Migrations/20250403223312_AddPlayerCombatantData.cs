80a0cf46348841a8b5a38b9c319c74d75dcecd60
dargall <dargall91@comcast.net>
2025-04-03 18:33:54 -0400

    Chore: Add migration for player combatant data

---
+++ 5eTools.Data/Migrations/20250403223312_AddPlayerCombatantData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _5eTools.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayerCombatantData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InitiativeBonus",
+                table: "PlayerCharacter",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "InitiativeRoll",
+                table: "PlayerCharacter",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDead",
+                table: "PlayerCharacter",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InitiativeBonus",
+                table: "PlayerCharacter");
+
+            migrationBuilder.DropColumn(
+                name: "InitiativeRoll",
+                table: "PlayerCharacter");
+
+            migrationBuilder.DropColumn(
+                name: "IsDead",
+                table: "PlayerCharacter");
+        }
+    }
+}
