1cdbf4c1fac1dcb7afa0f9ff871a620c2aa32288
dargall <dargall91@comcast.net>
2025-04-03 12:58:26 -0400

    Chore: Add migrations to support music looping

---
+++ 5eTools.Data/Migrations/20250403164907_AddMusicLooping.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _5eTools.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMusicLooping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "LoopEndTime",
+                table: "Music",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "LoopStartTime",
+                table: "Music",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoopEndTime",
+                table: "Music");
+
+            migrationBuilder.DropColumn(
+                name: "LoopStartTime",
+                table: "Music");
+        }
+    }
+}
